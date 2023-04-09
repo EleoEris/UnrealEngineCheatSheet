b73c3be5119ca9878efcb281ecfbae12e34b723c
KaT <79694371+EleoEris@users.noreply.github.com>
2023-04-09 15:24:14 +0200

    added descriptions and fixed a moveAtmosphereUp bug

--- UnrealEditorPyUtil.py
+++ UnrealEditorPyUtil.py
@@ -1,22 +1,29 @@
+import unreal
 
 # untested - case insensitive
-def getActorsByNames(*args) -> [actor]:
+# takes strings and yields all level actors that contain those strings in their get_name()
+def getActorsByNames(*args) -> list[actor]:
     for actor in unreal.EditorLevelLibrary.get_all_level_actors():
         if any(string.lower() in lightActor.get_name().lower() for string in *args):
             yield actor
 # tested - single line for editor
 # [lightActor for lightActor in unreal.EditorLevelLibrary.get_all_level_actors() if any(string.lower() in lightActor.get_name().lower() for string in ["skylight", "skyatmosphere", "directionallight", "exponentialheightfog"])]
 
+
 # untested - case sensitive
-def getActorsByNamesCS(*args) -> [actor]:
+# takes strings and yields all level actors that contain those strings in their get_name()
+def getActorsByNamesCS(*args) -> list[actor]:
     for actor in unreal.EditorLevelLibrary.get_all_level_actors():
         if any(string in lightActor.get_name().lower() for string in *args):
             yield actor
+# tested - single line for editor
+# [lightActor for lightActor in unreal.EditorLevelLibrary.get_all_level_actors() if any(string in lightActor.get_name() for string in ["SkyLight", "SkyAtmosphere", "DirectionalLight", "ExponentialHeightFog"])]
+
 
 # untested
-def moveAtmosphereUp():
-    for actor in getActorsByNames("skylight", "skyatmosphere", "directionallight", "exponentialheightfog"):
-        actor.set_actor_location(unreal.Vector(0, 0, 3000+(100*i)), False, True)
-
+# moves the atmospheric actors ["skylight", "skyatmosphere", "directionallight", "exponentialheightfog"] to the coordinates x, y coordinates 0, 0 and up
+def moveAtmosphereUp(baseHeight = 3000, heightDelta = 100) -> None:
+    for i, actor in enumerate(getActorsByNames("skylight", "skyatmosphere", "directionallight", "exponentialheightfog")):
+        actor.set_actor_location(unreal.Vector(0, 0, baseHeight+(heightDelta*i)), False, True)
 # tested - single line for editor:
 # for i, actor in enumerate([lightActor for lightActor in unreal.EditorLevelLibrary.get_all_level_actors() if any(string.lower() in lightActor.get_name().lower() for string in ["skylight", "skyatmosphere", "directionallight", "exponentialheightfog"])]): actor.set_actor_location(unreal.Vector(0, 0, 3000+(100*i)), False, True)
